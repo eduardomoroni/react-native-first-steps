5c89c0f50a9a8d440d6255fa2be2c6eee17bd313
Eduardo Moroni <eduardomoroni@gmail.com>
2017-02-27 16:24:56 -0300

    Facebook Login on iOS

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
+const FBSDK = require('react-native-fbsdk')
+const {
+  LoginButton,
+  AccessToken
+} = FBSDK
 
 export default class App extends Component {
-  render() {
+  render () {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -26,8 +31,26 @@ export default class App extends Component {
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
+        <LoginButton
+          publishPermissions={['publish_actions']}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert('login has error: ' + result.error)
+              } else if (result.isCancelled) {
+                alert('login is cancelled.')
+              } else {
+                AccessToken.getCurrentAccessToken().then(
+                  (data) => {
+                    alert(data.accessToken.toString())
+                  }
+                )
+              }
+            }
+          }
+          onLogoutFinished={() => alert('logout.')} />
       </View>
-    );
+    )
   }
 }
 
@@ -36,16 +59,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
