5385e5c19128785c3442a735907b031b4e202b19
Eduardo Moroni <eduardomoroni@gmail.com>
2017-03-28 09:42:47 -0300

    Adding Color bar

--- src/Containers/CardSearchForm.js
+++ src/Containers/CardSearchForm.js
@@ -3,15 +3,13 @@ import { reduxForm, Field, formValueSelector } from 'redux-form'
 import { connect } from 'react-redux'
 import { realm } from '../Config/Realm'
 import { inheritanceToArray } from '../Realm/Conversion/Realm-utils'
-import { Metrics } from '../Styles/Themes'
 import { searchForCards } from '../Redux/Actions'
+import Styles from '../Styles/CardSearchFormStyle'
 import TextInputForm from './Components/TextInputForm'
 import DropdownInputForm from './Components/DropdownInputForm'
 import SubmitButtonForm from './Components/SubmitButtonForm'
 import ManaIconsBar from './Components/ManaIconsBar'
-
 import {
-  StyleSheet,
   View,
   TouchableOpacity,
   Keyboard
@@ -38,7 +36,8 @@ let CardSearchForm = (props: CardSearchFormProps) => {
 
   const {
     cardType,
-    cardSubType
+    cardSubType,
+    cardColor
   } = props
 
   const submit = values => {
@@ -55,7 +54,7 @@ let CardSearchForm = (props: CardSearchFormProps) => {
           <Field name='cardSubType' component={DropdownInputForm} dropdownItems={cardSubTypes} selectedValue={cardSubType} />
         )}
         <Field name='cardText' component={TextInputForm} />
-        <ManaIconsBar />
+        <Field name='cardColors' component={ManaIconsBar} selectedColors={cardColor} />
       </View>
       <TouchableOpacity style={Styles.containerFooter} onPress={handleSubmit(submit)} >
         <SubmitButtonForm onPress={handleSubmit(submit)} />
@@ -82,37 +81,8 @@ Field.propTypes = {
   selectedValue: React.PropTypes.string
 }
 
-const Styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  formContainer: {
-    marginHorizontal: Metrics.marginHorizontal,
-    paddingTop: Metrics.navBarHeight + Metrics.smallMargin
-  },
-  containerFooter: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: 0
-  },
-  twoFieldsPerLine: {
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  leftField: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    marginRight: Metrics.smallMargin / 2
-  },
-  rightField: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    marginLeft: Metrics.smallMargin / 2
-  }
-})
-
 const mapStateToProps = (state) => {
+  const cardColor = selector(state, 'cardColor') || []
   const cardTypes = inheritanceToArray(realm.objects('Type').snapshot())
   const cardSubtypes = inheritanceToArray(realm.objects('SubType').snapshot())
 
@@ -125,7 +95,8 @@ const mapStateToProps = (state) => {
     cardName: selector(state, 'cardName'),
     cardType: selector(state, 'cardType'),
     cardSubType: selector(state, 'cardSubType'),
-    cardText: selector(state, 'cardText')
+    cardText: selector(state, 'cardText'),
+    cardColor: cardColor
   }
 }
 
--- src/Containers/Components/ManaIconsBar.js
+++ src/Containers/Components/ManaIconsBar.js
@@ -3,18 +3,38 @@
 import React, { Component } from 'react'
 import Styles from '../../Styles/FormStyle'
 import ManaSymbol, { ValidColors } from './ManaSymbol'
+import { connect } from 'react-redux'
+import { change as changeFieldValue } from 'redux-form'
+import I18n from 'react-native-i18n'
+import _ from 'lodash'
 import {
   View,
   Text,
   TouchableOpacity
 } from 'react-native'
 
-export default class ManaIconsBar extends Component {
+class ManaIconsBar extends Component {
   render () {
+    const { selectedColors, updateColors } = this.props
+
+    function toggleColor (color) {
+      let updatedSelect = []
+
+      if (selectedColors.includes(color)) {
+        updatedSelect = _.without(selectedColors, color)
+      } else {
+        updatedSelect = _.concat(selectedColors, color)
+      }
+
+      updateColors(updatedSelect)
+    }
+
     const renderManaSymbol = (color) => {
+      const isSelected = selectedColors.includes(color)
+
       return (
-        <TouchableOpacity key={color} >
-          <ManaSymbol style={Styles.manaIcon} color={color} />
+        <TouchableOpacity key={color} onPressOut={() => toggleColor(color)} >
+          <ManaSymbol color={color} style={[Styles.manaIcon, {opacity: isSelected ? 1 : 0.25}]} />
         </TouchableOpacity>
       )
     }
@@ -22,10 +42,22 @@ export default class ManaIconsBar extends Component {
     return (
       <View style={Styles.container}>
         <Text style={Styles.text}>
-          Cor:
+          {I18n.t('color')}
         </Text>
         { ValidColors.map(renderManaSymbol) }
       </View>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {}
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateColors: (colors) => dispatch(changeFieldValue('CardSearchForm', 'cardColor', colors))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManaIconsBar)
